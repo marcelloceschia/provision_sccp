c1a614561ad374f1dce620b839833da7fb06bb36
Marcello Ceschia <marcello.ceschia@gmail.com>
2020-03-17 21:04:31 +0100

    load certificate data

--- lib/crypto.php
+++ lib/crypto.php
@@ -3,14 +3,21 @@ declare(strict_types=1);
 
 namespace SCCP\Crypto;
 
-class HashAlgo extends Enum {
+class HashAlgo {
     private const SHA1 = 0;
     private const SHA256 = 1;
 }
 
 class Certificate {
     function __construct(string $file) {
-		$this->file = $file;
+        $this->file = $file;
+
+
+        $cert = openssl_x509_read( file_get_contents($this->file) );
+        $this->certData = openssl_x509_parse( $cert );
+        //print_r( $this->certData );
+        //echo openssl_x509_fingerprint($cert);
+        openssl_x509_free( $cert );
     }
     
     public function hashAsBase64(int $hashAlg)  : string  {
